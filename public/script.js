dfefe956779cb757f8b0093e1c209b3775a1da8b
Dara <dxm190032@utdallas.edu>
2023-06-08 16:39:16 -0500

    Graphs fixed, learn more button fixed
    DONE:
    - graphs finally fixed
    - learn more opens modal that allows for more information
    - learn more modal works fully
    - graphs have axis
    
    NOT DONE:
    - graph tooltip not working
    - more user options
    
    AFTER:
    - guide in README.md
    - work on moving data from previous classes

--- public/script.js
+++ public/script.js
@@ -1,6 +1,6 @@
 // set the dimensions and margins of the graph
-var margin = {top: 20, right: 20, bottom: 20, left: 50},
-width = 350 - margin.left - margin.right,
+var margin = {top: 50, right: 50, bottom: 40, left: 50},
+width = 400 - margin.left - margin.right,
 height = 250 - margin.top - margin.bottom;
 
 // set the ranges
@@ -12,24 +12,38 @@ var valueline = d3.line()
    .x(function(d) { return x(d.year); })
    .y(function(d) { return y(d.population); });
 
-// append the svg obgect to the body of the page
-// appends a 'group' element to 'svg'
-// moves the 'group' element to the top left margin
+// append the svg to the svgcontainerSecondary
 var svg = d3.select("#svgcontainerSecondary").append("svg")
-   .attr("width", width + margin.left + margin.right)
-   .attr("height", height + margin.top + margin.bottom)
-   .append("g").attr("transform",
-      "translate(" + margin.left + "," + margin.top + ")");
+   .attr("width", width + margin.left + margin.right) // determines width
+   .attr("height", height + margin.top + margin.bottom) // determines height
+   .append("g")
+   .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+   .on("mouseover", function(d) {
+    d3.select("#tooltipA")
+    .transition()
+    .duration(200)
+    .style("opacity", 1)
+    .text(d.population);
+   })
+   .on("mouseout", function(d) {
+    d3.select("#tooltipA").style("opacity", 0)
+   })
+   .on("mousemove", function(d) {
+    d3.select("#tooltipA")
+    .style("left", d3.event.pageX + "px")
+    .style("top", d3.event.pageY + "px")
+   });
 
 // Get the data
 d3.csv("data.csv", function(error, data) {
    if (error) throw error;
    // format the data
+   var parseDateA = d3.timeParse("%Y");
    data.forEach(function(d) {
-      d.year = d.year;
+      d.year = parseDateA(d.year);
       d.population = +d.population;
    });
-
+  
    // Scale the range of the data
    x.domain(d3.extent(data, function(d) { return d.year; }));
    y.domain([0, d3.max(data, function(d) { return d.population; })]);
@@ -45,15 +59,54 @@ d3.csv("data.csv", function(error, data) {
       .attr("transform", "translate(0," + height + ")")
       .call(d3.axisBottom(x));
 
-   // Add the Y Axis
-   svg.append("g")
-      .call(d3.axisLeft(y));
+  // Add the Y Axis
+  svg.append("g")
+    .call(d3.axisLeft(y));
+  
+  
+   // Add the text for X-axis
+  const xLabel = svg.append("text")
+    .attr("x", width/2)
+    .style("font-size", "16px")
+    .attr("y", height+35)
+    .style("text-anchor", "middle")
+    .text("Year");
+
+   // Add the text for Y-axis
+  const yLabel = svg.append("text")
+    .attr("transform", "rotate(-90,15,"+(height/2)+")")
+    .attr("x", 0)
+    .attr("y", height/2-50)
+    .style("text-anchor", "middle")
+    .text("Population (Millions)");
 });
+
+
+// -------- FIRST GRAPH - TOOLTIP -------- //
+
+var tooltip = d3.select("#primaryTooltip")
+  .append("div")
+  .attr("id", "tooltipA")
+  .attr("style", "position: absolute; opacity: 0");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // --------------- SECOND GRAPH --------------- //
 
 // set the dimensions and margins of the graph
-var margin2 = { top: 20, right: 20, bottom: 20, left: 50 },
+var margin2 = { top: 20, right: 20, bottom: 40, left: 50 },
   width2 = 500 - margin2.left - margin2.right,
   height2 = 250 - margin2.top - margin2.bottom;
 
@@ -79,6 +132,22 @@ var svg2 = d3
   .attr("height", height2 + margin2.top + margin2.bottom)
   .append("g")
   .attr("transform", "translate(" + margin2.left + "," + margin2.top + ")");
+
+// Add the text for X-axis
+const xLabel2 = svg2.append("text")
+  .attr("x", width2/2)
+  .style("font-size", "16px")
+  .attr("y", height2+35)
+  .style("text-anchor", "middle")
+  .text("Months - 2011-2012");
+  
+// Add the text for Y-axis
+const yLabel2 = svg2.append("text")
+.attr("transform", "rotate(-90,15,"+(height2/2)+")")
+.attr("x", 0)
+.attr("y", height2/2-50)
+.style("text-anchor", "middle")
+.text("Temperature");
 
 // Get the data
 d3.csv("temperature.csv", function (error, data2) {
@@ -111,6 +180,8 @@ d3.csv("temperature.csv", function (error, data2) {
     return d2.temperature;
   })]);
 
+  xAxis = d3.axisBottom().scale(x2).tickFormat(d3.timeFormat("%b"));
+
   // Add the valueline2 path.
   svg2.append("path")
     .data([data2])
@@ -120,12 +191,15 @@ d3.csv("temperature.csv", function (error, data2) {
 
   // Add the X Axis
   svg2.append("g")
+    .attr("class", "axis")
     .attr("transform", "translate(0," + height2 + ")") // Use height2 here
-    .call(d3.axisBottom(x2));
+    .call(xAxis);
+    //.call(d3.axisBottom(x2));
 
   // Add the Y Axis
   svg2.append("g")
     .call(d3.axisLeft(y2));
+
 
 
 });
