6d1e7e2851943e918f57d886d501e4c5a13f2509
Dara <dxm190032@utdallas.edu>
2023-06-12 14:58:49 -0500

    Finished the tooltip.
    DONE:
    - Tooltip finally shows the right value
    - No errors from tooltip
    
    NOT DONE:
    - Reactive site function "updateSvgDImensions()" is incomplete
    - Fix hotbar (perhaps transition away from bootstrap?)
    
    AFTER:
    - Make code generalized enough so that new people can use

--- public/script.js
+++ public/script.js
@@ -41,7 +41,7 @@ d3.csv("data.csv", function(error, data) {
    var parseDateA = d3.timeParse("%Y");
    data.forEach(function(d) {
       d.year = parseDateA(d.year);
-      d.population = +d.population;
+      d.population = +d.population || 0;
    });
   
    // Scale the range of the data
@@ -84,7 +84,7 @@ d3.csv("data.csv", function(error, data) {
   var tooltipA = d3.select("#secondaryTooltip");
 
   // Function to show the tooltip
-  function showTooltipA(event, d) {
+  function showTooltipA(d) {
     tooltipA
       .style("left", margin.left + "px")
       .style("top", margin.top + "px")
@@ -110,8 +110,8 @@ d3.csv("data.csv", function(error, data) {
     })
     .attr("r", 4) // Customize the radius of the circles as needed
     .attr("fill", "steelblue")
-    .on("mouseover", function (event, d) {
-      showTooltipA(event, d);
+    .on("mouseover", function (d) {
+      showTooltipA(d);
     })
     .on("mouseout", function () {
       hideTooltipA();
@@ -129,17 +129,12 @@ d3.csv("data.csv", function(error, data) {
 // --------------- SECOND GRAPH --------------- //
 
 // set the dimensions and margins of the graph
-var margin2 = { top: 20, right: 20, bottom: 40, left: 50 },
-  width2 = 500 - margin2.left - margin2.right,
+var margin2 = { top: 50, right: 50, bottom: 40, left: 50 },
+  width2 = 600 - margin2.left - margin2.right,
   height2 = 250 - margin2.top - margin2.bottom,
   default_ratio2 = width2/height2;
-
-// Check to see if dimensions are to be changed due to window size
-if (current_ratio > default_ratio2) {
-  width2 = window_width - 100 - margin2.right;
-  height2 = window_height - margin2.top - margin2.bottom - 330;
-}
-
+  
+var xAxis = 0;
 
 // set the ranges
 var x2 = d3.scaleTime().range([0, width2]);
@@ -213,6 +208,7 @@ d3.csv("temperature.csv", function (error, data2) {
 
   xAxis = d3.axisBottom().scale(x2).tickFormat(d3.timeFormat("%b"));
   var formatTime = d3.timeFormat("%b");
+  var formatTime2 = d3.timeFormat("%B %d, %Y");
 
   // Add the valueline2 path.
   svg2.append("path")
@@ -223,25 +219,26 @@ d3.csv("temperature.csv", function (error, data2) {
 
   // Add the X Axis
   svg2.append("g")
-    .attr("class", "axis")
+    .attr("class", "x-axis")
     .attr("transform", "translate(0," + height2 + ")") // Use height2 here
     .call(xAxis);
     //.call(d3.axisBottom(x2));
 
   // Add the Y Axis
   svg2.append("g")
+    .attr("class", "y-axis") // Add a class to the Y-axis element
     .call(d3.axisLeft(y2));
 
   // Create the tooltip reference
 var tooltip = d3.select("#primaryTooltip");
 
 // Function to show the tooltip
-function showTooltip(event, d2) {
+function showTooltip(d2) {
   tooltip
     .style("left", margin2.left + "px")
     .style("top", margin2.top + "px")
     .style("opacity", 1)
-    .html(formatTime(d2.date) + "<br/>" + d2.temperature);
+    .html(formatTime2(d2.date) + "<br/>" + "Temp: " + +d2.movingAverage);
     //.html("Temperature: " + d2.temperature);
   }
 
@@ -263,8 +260,8 @@ svg2.selectAll("circle")
   })
   .attr("r", 4) // Customize the radius of the circles as needed
   .attr("fill", "steelblue")
-  .on("mouseover", function (event, d2) {
-    showTooltip(event, d2);
+  .on("mouseover", function (d2) {
+    showTooltip(d2);
   })
   .on("mouseout", function () {
     hideTooltip();
@@ -273,3 +270,57 @@ svg2.selectAll("circle")
 
 
 });
+
+// Function to update the SVG dimensions based on the container size
+function updateSvgDimensions() {
+  var containerWidth = d3.select("#svgcontainerPrimary").node().getBoundingClientRect().width;
+  var containerHeight = d3.select("#svgcontainerPrimary").node().getBoundingClientRect().height;
+
+  // Adjust the dimensions based on the screen size
+  var mobileWidth = 400;
+  var mobileHeight = 200;
+  
+  if (containerWidth < mobileWidth) {
+    width2 = containerWidth - margin2.left - margin2.right;
+    height2 = mobileHeight - margin2.top - margin2.bottom;
+  } else {
+    width2 = 600 - margin2.left - margin2.right;
+    height2 = 250 - margin2.top - margin2.bottom;
+  }
+
+  // Update the SVG attributes
+  svg2.attr("width", containerWidth)
+    .attr("height", containerHeight);
+  
+  // Update the x and y scales
+  x2.range([0, containerWidth - margin2.left - margin2.right]);
+  y2.range([containerHeight - margin2.top - margin2.bottom, 0]);
+
+  // Update the X and Y axes
+  svg2.select(".x-axis")
+    .attr("transform", "translate(0," + (containerHeight - margin2.top) + ")")
+    .call(xAxis);
+  
+  svg2.select(".y-axis")
+    .call(d3.axisLeft(y2));
+  
+  // Update the position of the X-axis label
+  xLabel2.attr("x", containerWidth / 2)
+    .attr("y", containerHeight + 35);
+  
+  // Redraw the line and circles
+  svg2.select(".line")
+    .attr("d", valueline2);
+  
+  svg2.selectAll("circle")
+    .attr("cx", function (d2) {
+      return x2(d2.date);
+    })
+    .attr("cy", function (d2) {
+      return y2(d2.movingAverage);
+    });
+}
+
+// Call the updateSvgDimensions function initially and on window resize
+updateSvgDimensions();
+d3.select(window).on("resize", updateSvgDimensions);
